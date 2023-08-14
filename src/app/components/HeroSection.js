2bb6f10cd8442dde7dce28481ef3b58fc7bfcf26
SUBHAM-BANERJEE-2003 <subhambanerjee2003@gmail.com>
2023-08-14 13:00:43 +0530

    added carousel

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -1,14 +1,32 @@
-import React from 'react'
+"use client"
 
-
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 const HeroSection = () => {
   return (
     <>
-    <main >
-      <h1 className='dummy'>HII</h1>
-    </main>
+            <div>
+              <Carousel>
+                  <div>
+                      <img src="/hero-1.jpeg" alt="image1"/>
+                      <p className="legend">Schedule Video Calls from anywhere in the earth</p>
+  
+                  </div>
+                  <div>
+                      <img src="/hero-2.jpg" alt="image2" />
+                      <p className="legend">Event Hosting Facilities Available</p>
+  
+                  </div>
+                  <div>
+                      <img src="/hero-3.jpg" alt="image3"/>
+                      <p className="legend">ChatRoom facilities in both mobile and PC</p>
+  
+                  </div>
+              </Carousel>
+            </div>
     </>
-  )
+  );
 }
 
 export default HeroSection
